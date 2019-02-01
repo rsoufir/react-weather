6b5712a2db9c55056d7daf1009292787d36a7dff
Ruben <ruben.soufir@gmail.com>
2019-02-01 11:48:21 +0100

    init App.js project

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+
   render() {
+    const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {isLoading ? (
+          <Text>Recherche de la météo en cours...</Text>
+        ) : (
+          <View>
+            <Text>React Weather App</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -16,6 +27,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
